abbc3619c0c55e0204fc90bb59e329e519144d32
Damilola Babatunde <babatundedamilola334@gmail.com>
2024-01-17 12:08:43 +0100

    'changes'

--- src/Pages/productcategories/Productcategories.jsx
+++ src/Pages/productcategories/Productcategories.jsx
@@ -3,14 +3,25 @@ import axios from "axios";
 
 const Productcategories = () => {
   const [data, setData] = useState("");
+  const[loading, setLoading] = useState(false);
   console.log("data", data);
 
   const getProductCategory = async () => {
+    setLoading(true); 
+    // or setLoading(!Loading)
+    try{
     const response = await axios.get(
       "https://dummyjson.com/products/categories"
     );
     console.log("res", response.data);
     setData(response.data);
+    setLoading(false);
+    // toast notification of success message - e.g "Login successful."
+    }catch(err){
+      console.log(err);
+    //  toast notification of error message
+    setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -19,7 +30,7 @@ const Productcategories = () => {
 
   return (
     <div>
-      {data &&
+        {loading ? <p>Loading</p> : data &&
         data?.map((val, index) => {
           return (
             <div key={index}>
